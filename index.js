ac755baa0dcc7001071f5227b79426f024c7b22e
psirenny <djtorres0@gmail.com>
2013-12-07 14:54:14 -0500

    syntax error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (options) {
   return function (req, res, next) {
     var model = req.getModel();
     var language = req.headers['accept-language'] || '';
-    model.push($locale.preffered, language.split(','));
+    model.push('$locale.preferred', language.split(','));
     next();
   };
 };
