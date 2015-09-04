774637b2c6fec21b0f6ff266ff4b67e97f68e0a2
psirenny <djtorres0@gmail.com>
2015-09-04 11:49:56 -0400

    Check if the model has been closed.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+'use strict';
+
 module.exports = function (options) {
   if (!options) options = {};
   if (!options.path) options.path = '$locale';
   if (!options.name) options.name = 'browser';
+
   return function (req, res, next) {
     var model = req.getModel();
     var path = options.path + '.strategies.' + options.name;
     var strategy = {};
     var locales = req.get('accept-language');
+    if (!model) return next();
+
     locales = locales ? locales.split(',') : [];
     strategy.order = options.order;
     strategy.locales = locales;
